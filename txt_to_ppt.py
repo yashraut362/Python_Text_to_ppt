00a2de9dcba892f9cb6991c119772aa2298445bd
yashraut362 <yashraut362@gmail.com>
2020-08-29 16:28:22 +0530

    table added

--- txt_to_ppt.py
+++ txt_to_ppt.py
@@ -27,7 +27,33 @@ str = open(input('Give Path for Text file or drop File here '), 'r').read()
 p.text = str
 p.level = 2
 
-blank_slide_layout = prs.slide_layouts[2]
+
+slide = prs.slides.add_slide(prs.slide_layouts[2])
+table_placeholder = slide.shapes[1]
+shapes = slide.shapes
+
+shapes.title.text = 'Adding a Table'
+
+rows = cols = 2
+left = top = Inches(2.0)
+width = Inches(6.0)
+height = Inches(0.8)
+
+table = shapes.add_table(rows, cols, left, top, width, height).table
+
+# set column widths
+table.columns[0].width = Inches(2.0)
+table.columns[1].width = Inches(4.0)
+
+# write column headings
+table.cell(0, 0).text = 'Foo'
+table.cell(0, 1).text = 'Bar'
+
+# write body cells
+table.cell(1, 0).text = 'Baz'
+table.cell(1, 1).text = 'Qux'
+
+blank_slide_layout = prs.slide_layouts[3]
 slide = prs.slides.add_slide(blank_slide_layout)
 
 # uncomment the below line if you want to give custom image.
@@ -39,5 +65,7 @@ left = Inches(0.5)
 height = Inches(5)
 top = Inches(0.5)
 pic = slide.shapes.add_picture(img_path, left, top, height=height)
+
+
 
 prs.save('{slidename}.pptx'.format(slidename=input("Enter Name for PPT ")))
